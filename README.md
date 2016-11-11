1081c3cc7f53de921c93a1a78d10a8fd033a24f7
Sam <sam@mageplaza.com>
2016-11-11 06:47:07 -0800

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Magento 2 Spanish Language Pack
+
+Read more [Magento 2 Spanish Language Pack](https://www.mageplaza.com/magento-2-spanish-language-pack.html)
+
+## How to Install
+
+
+### via Composer
+
+**Install Spanish pack**:
+
+```
+composer require mageplaza/magento-2-spanish-language-pack:*
+php bin/magento cache:clean
+php bin/magento setup:static-content:deploy es-es
+
+```
+
+
+Ref: https://www.mageplaza.com/magento-2-spanish-language-pack.html
---
+++ registration.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *                     Mageplaza_Spanish package
+ *                     NOTICE OF LICENSE
+ *
+ *                     This source file is subject to the MIT License
+ *                     that is bundled with this package in the file LICENSE.txt.
+ *                     It is also available through the world-wide-web at this URL:
+ *                     https://www.mageplaza.com/LICENSE.txt
+ *
+ *                     @category  Mageplaza
+ *                     @package   Mageplaza_Spanish
+ *                     @copyright Copyright (c) 2016
+ *                     @license   https://www.mageplaza.com/LICENSE.txt
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'mageplaza_es_es',
+    __DIR__
+);
